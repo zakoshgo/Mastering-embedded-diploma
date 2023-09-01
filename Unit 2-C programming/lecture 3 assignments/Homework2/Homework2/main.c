4e526842e779252cff9a471d7a982058b82b929f
ziad ashraf <81923383+zakoshgo@users.noreply.github.com>
2023-09-01 09:58:47 +0300

    Homework 2 ex 1-3

---
+++ Unit 2-C programming/lecture 3 assignments/Homework2/Homework2/main.c
@@ -0,0 +1,56 @@
+/*Example 1*/
+/*
+#pragma warning(disable: 4996)
+#include<stdio.h>
+void main() {
+	int num;
+	printf("Enter an integer you want to check: ");
+	scanf("%d", &num);
+	if (num % 2 == 0)
+		printf("%d is even ",num);
+	else
+		printf("%d is odd ",num);
+}
+*/
+
+/*Example 2*/
+/*
+#pragma warning(disable: 4996)
+#include<stdio.h>
+void main() {
+	char U_char;
+	printf("Enter an alphabet: ");
+	scanf("%c", &U_char);
+	if (U_char == 'a' || U_char == 'e' || U_char == 'o' || U_char == 'u' || U_char == 'i')
+		printf("%c is a vowel", U_char);
+	else
+		printf("%c is a consonant", U_char);
+}
+*/
+
+/*Example 3*/
+/*
+#pragma warning(disable: 4996)
+#include<stdio.h>
+void main() {
+	float num1, num2, num3;
+	printf("Enter three numbers: ");
+	scanf("%f %f %f", &num1, &num2, &num3);
+	if (num1 > num2) {
+		if (num1 > num3)
+			printf("Largest number = %f", num1);
+		else
+			printf("Largest number = %f", num3);
+	}
+	else
+	{
+		if (num2 > num3)
+			printf("Largest number = %f", num2);
+		else
+			printf("Largest number = %f", num3);
+	}
+
+
+}
+*/
+
